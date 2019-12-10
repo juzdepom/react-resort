8d4569198fa91fc6cb32026a37bf7425f024bf28
Julia Gao Miller <julia.gao.miller@gmail.com>
2019-12-10 10:07:23 +0700

    set up Banner component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 
 function Home() {
     return (
-        <Hero> </Hero>
+        <Hero>
+            <Banner title="Luxurious Rooms" subtitle="Deluxe rooms starting at $299">
+                <Link to='/rooms' className="btn-primary">Our Rooms</Link>
+            </Banner>
+        </Hero>
     )
 }
 
