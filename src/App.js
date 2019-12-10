3154f14dee8976639e187a47012bf6f9b3ad9957
Julia Gao Miller <julia.gao.miller@gmail.com>
2019-12-10 09:25:31 +0700

    set up react-router-dom foundation

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+import { Route, Switch } from 'react-router-dom';
+
 function App() {
   return (
     <>
-    <Home></Home>
-    <Rooms></Rooms>
-    <SingleRoom></SingleRoom>
-    <Error></Error>
+      {/* LEARN: the Switch component allows us to render the error page */}
+      <Switch>
+        {/* LEARN: if you don't have the 'exact' attribute, /rooms/ will also render the Home component because it includes '/' */}
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/rooms/" component={Rooms}/>
+        {/* LEARN: ':slug' is the 'url parameters' */}
+        <Route exact path="/rooms/:slug" component={SingleRoom}/>
+        {/* LEARN: without the Switch component, the Error component would show up in each and every route */}
+        <Route component={Error}/>
+      </Switch>
     </>
   );
 }
